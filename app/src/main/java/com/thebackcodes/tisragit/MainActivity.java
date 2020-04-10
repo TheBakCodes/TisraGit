ce3d36104b469c741941474cf39093421b256ff3
TheBakCodes <63441288+TheBakCodes@users.noreply.github.com>
2020-04-10 12:49:37 +0530

    Update MainActivity.java

--- app/src/main/java/com/thebackcodes/tisragit/MainActivity.java
+++ app/src/main/java/com/thebackcodes/tisragit/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //change1
+        
+        
+        //change2 from github editor
     }
 }
